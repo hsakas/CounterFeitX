27b32b0ee529adfa1caf4b400636f23252ecffff
hsakas <algortroy@gmail.com>
2020-07-21 14:30:47 +0530

    missing data algo modified

--- missing_sim_main.py
+++ missing_sim_main.py
@@ -8,7 +8,7 @@ from annoy import AnnoyIndex
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from textsim.utils import EmbedSentence, SentenceDataset
+from textsim.utils import EmbedSentence, SentenceDataset, SentencesDataset
 
 # create a sentence embedder
 embedder = EmbedSentence()
@@ -18,14 +18,18 @@ df = pd.read_csv('missing_data.csv')
 # create a dataloader
 sentence_dataset = SentenceDataset(sentences=df['title'].to_numpy())
 desc_dataset = SentenceDataset(sentences=df['description'].to_numpy())
+dataset = SentencesDataset(sentences_list=df.filter(['title', 'description']).to_numpy())
+# anything in the column
 
 sentence_dataloader = DataLoader(dataset=sentence_dataset, batch_size=10, shuffle=False)
 desc_dataloader = DataLoader(dataset=desc_dataset, batch_size=10, shuffle=False)
+dataloader = DataLoader(dataset=dataset, batch_size=10, shuffle=False)
 
 
 # create a annoy tree
 title_tree = AnnoyIndex(100, 'angular')
 desc_tree = AnnoyIndex(100, 'angular')
+tree = AnnoyIndex(100, 'angular')
 
 
 if __name__ == '__main__':
@@ -41,10 +45,23 @@ if __name__ == '__main__':
             desc_tree.add_item(counter_j, vec)
             counter_j += 1
 
+    counter_k = 0
+    for batch in tqdm(dataloader):
+        for _, vec in enumerate(batch):
+            tree.add_item(counter_k, vec)
+            counter_k += 1
+
     # build the tree
     title_tree.build(100)
     desc_tree.build(100)
+    tree.build(100)
 
     # query using a index
     print(title_tree.get_nns_by_item(0, 10))
     print(desc_tree.get_nns_by_item(0, 10))
+    print(tree.get_nns_by_item(0, 10))
+
+    for i in tree.get_nns_by_item(164, 2):
+        print(f'id -> {i}')
+        print('value -> ' + df.filter(['title', 'description']).iloc[i])
+        print('brand suggestion -> ', df.filter(['brand']).iloc[i])
